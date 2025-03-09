1f2c6e370068c8bdd74c9259c0a04aea0a9cf452
Pushpesh <ppant9418@gmail.com>
2025-03-10 00:01:33 +0530

    for deployment

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -8,7 +8,7 @@ export const generateTokenAndSetCookie = (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "lax", // changed strict to lax
-    secure: false //process.env.NODE_ENV !== "development", (Will change it later)
+    secure: process.env.NODE_ENV !== "development"
   });
   return token; // for debugging
 };
