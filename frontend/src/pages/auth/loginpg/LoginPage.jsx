0a7140f7058800b70e8177d6d73bcaccb736adb3
Pushpesh <ppant9418@gmail.com>
2025-03-10 12:14:02 +0530

    Added callback url

--- frontend/src/pages/auth/loginpg/LoginPage.jsx
+++ frontend/src/pages/auth/loginpg/LoginPage.jsx
@@ -30,7 +30,7 @@ function LoginPage() {
     }
   };
   const handleGoogleLogin = () => {
-    window.location.href = "http://localhost:5000/api/auth/google";
+    window.location.href = `${import.meta.env.VITE_API_URL}/api/auth/google`;
  // Redirects to Google OAuth
   };
   return (
